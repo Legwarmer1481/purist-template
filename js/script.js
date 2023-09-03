307152b2efb617ed240e9e439a4241a72b51f1ac
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2023-09-03 13:58:38 -0400

    JS created

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Class Main that runs the other JavaScript codes.
+ */
+class Main{
+    constructor(){
+        this.init();
+    }
+    init(){
+        console.log('JavaScript is loaded !')
+    }
+}
+
+new Main();
