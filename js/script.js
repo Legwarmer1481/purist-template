be60650ced61ef52d0a86053d7649e5966b962ca
Legwarmer1481 <70u3nulj@larknight.anonaddy.com>
2023-09-07 22:26:20 -0400

    Component added, new snippets, css updated

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+import Header from "./modules/Header.js";
 /**
  * Class Main that runs the other JavaScript codes.
  */
 class Main{
     constructor(){
+        this.componentList = {
+            Header,
+        }
         this.init();
     }
     init(){
         console.log('JavaScript is loaded !')
+        const components = document.querySelectorAll('[data-component]');
+
+        for (const element of components) {
+            const componentName = element.dataset.component;
+
+            if(this.componentList[componentName]){
+                new this.componentList[componentName](element);
+            }else{
+                console.warn(`The ${componentName} component doesn't exist !`);
+            }
+        }
     }
 }
 
